fc0089efb724aef7e2836d609aafd2e7b6c8cca4
Najrul <mdnajrulislam.4738@gmail.com>
2024-03-27 21:46:07 +0600

    I have installed heroicons/react and added navbar section

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import Container from "./Container";
+
+const Home = () => {
+  return (
+    <section>
+      <Container>
+        <div className=" w-full p-5 border-2 border-white rounded-lg">
+          <p>Home</p>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import Container from "./Container";
+import { UserIcon } from "@heroicons/react/24/solid";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Container>
+        <div className="flex justify-between items-center p-5">
+          <h1 className="text-xl md:text-2xl font-bold">Task Board</h1>
+          <div className="text-black bg-white rounded-full p-2">
+            <UserIcon class="h-6 w-6 " />
+          </div>
+        </div>
+      </Container>
+    </nav>
+  );
+};
+
+export default Navbar;
